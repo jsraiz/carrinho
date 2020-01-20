e6a88acb737b8a6742d0dee9c228657e3ea1718f
Ayrton Teshima <ayrtonteshima@gmail.com>
2020-01-20 20:21:01 -0200

    feat: redux extension

--- carrinho-react-redux-starter/src/index.js
+++ carrinho-react-redux-starter/src/index.js
@@ -8,7 +8,10 @@ import { Provider } from 'react-redux';
 
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
